1ddd66810959230adaf082c6e31138897cddac66
JohnBach <hoangbachtran2016@gmail.com>
2023-09-07 23:26:58 +0700

    Update Babysitter's Profile screen

--- src/screens/main/profile/BabysitterProfile.tsx
+++ src/screens/main/profile/BabysitterProfile.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Center, FormControl, HStack, Image, Input, Radio, Text, VStack, useTheme } from 'native-base';
+import { Avatar, Button, Center, HStack, Image, Text, VStack, useTheme } from 'native-base';
 import React from 'react'
 import { useDispatchApp, useSelectorApp } from '../../../store/redux/store';
 import { TouchableOpacity } from 'react-native';
@@ -12,7 +12,7 @@ const BabysitterProfile = () => {
 
   return (
     <Center flex={1} bg="white" safeArea>
-      <Center flex={1}>
+      <Center flex={1} justifyContent="flex-start" pt={10}>
         <Text color="primary.600" fontSize={24} fontWeight={500} mb={4}>
           Thông tin
         </Text>
@@ -34,72 +34,36 @@ const BabysitterProfile = () => {
           </TouchableOpacity>
         </HStack>
 
-        <HStack space={6}>
-          <VStack space={2}>
-            <Button
-              _text={{
-                w: 121,
-                maxWidth: 130
-              }}
-              justifyContent="center"
-              rightIcon={<ArrowRight2 color={useTheme().colors.primary[600]} />}
-              borderColor="primary.600"
-              variant="outline"
-              borderWidth={1}
-              borderRadius={6}
-              w={170}
-            >
-              Bảo mẫu
-            </Button>
-            <Button
-              _text={{
-                w: 121,
-                maxWidth: 130
-              }}
-              justifyContent="left"
-              rightIcon={<ArrowRight2 color={useTheme().colors.primary[600]} />}
-              borderColor="primary.600"
-              variant="outline"
-              borderWidth={1}
-              borderRadius={6}
-              w={170}
-            >
-              Quản lý trẻ
-            </Button>
-          </VStack>
-          <VStack space={2}>
-            <Button
-              _text={{
-                w: 121,
-                maxWidth: 130
-              }}
-              justifyContent="center"
-              rightIcon={<ArrowRight2 color={useTheme().colors.primary[600]} />}
-              borderColor="primary.600"
-              variant="outline"
-              borderWidth={1}
-              borderRadius={6}
-              w={170}
-            >
-              Yêu thích
-            </Button>
-            <Button
-              _text={{
-                w: 121,
-                maxWidth: 130
-              }}
-              justifyContent="left"
-              rightIcon={<ArrowRight2 color={useTheme().colors.primary[600]} />}
-              borderColor="primary.600"
-              variant="outline"
-              borderWidth={1}
-              borderRadius={6}
-              w={170}
-            >
-              Mật khẩu
-            </Button>
-          </VStack>
-        </HStack>
+        <VStack space={3} mt={16} w={365}>
+          <Button
+            _text={{
+              w: 305,
+            }}
+            justifyContent="center"
+            rightIcon={<ArrowRight2 color={useTheme().colors.primary[600]} />}
+            borderColor="primary.600"
+            variant="outline"
+            borderWidth={1}
+            borderRadius={6}
+            w="100%"
+          >
+            Bảo mẫu
+          </Button>
+          <Button
+            _text={{
+              w: 305,
+            }}
+            justifyContent="center"
+            rightIcon={<ArrowRight2 color={useTheme().colors.primary[600]} />}
+            borderColor="primary.600"
+            variant="outline"
+            borderWidth={1}
+            borderRadius={6}
+            w="100%"
+          >
+            Yêu thích
+          </Button>
+        </VStack>
       </Center>
 
       <TouchableOpacity
